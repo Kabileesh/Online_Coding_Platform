cc68f9956d545cdff37a50991060caee3cecf866
Kabileesh <gkabileesh@gmail.com>
2023-07-17 20:43:48 +0530

    cors fix 3

--- Backend/app.js
+++ Backend/app.js
@@ -10,11 +10,19 @@ credentials(passport);
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["*", "https://gilded-kleicha-f8ced8.netlify.app/"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["*", "https://gilded-kleicha-f8ced8.netlify.app/"],
+//   })
+// );
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
